0638a7009095d2c65daaa65dbab3cc6c71644579
Kerri L OGorman <kerri.l.ogorman@gmail.com>
2020-08-14 19:12:17 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -48,7 +48,7 @@ class Song
     find_by_name(name) || create(name) #returns (does not recreate) an existing song with the provided name if one exists in/ invokes .find_by_name instead of re-coding the same functionality/ creates a song if an existing match is not found/ invokes .create instead of re-coding the same functionality
   end
 
-  def self.new_from_filename(file)
+  def self.new_from_filename(filename)
     info = file.split(" - ")
 
   end
