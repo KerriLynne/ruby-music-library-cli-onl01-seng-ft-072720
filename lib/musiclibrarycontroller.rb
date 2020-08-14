ca51311b06b5ac9cace4533cd5a7c0ae66a06702
Kerri L OGorman <kerri.l.ogorman@gmail.com>
2020-08-14 21:06:25 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -62,16 +62,34 @@ class MusicLibraryController
   end
 
 
-  def play_song
-   sorted_songs = Song.all.sort_by{|song| song.name}
-   puts "Which song number would you like to play?"
-   list_songs
-   index = gets.strip.to_i - 1
+ #  def play_song
+ #   sorted_songs = Song.all.sort_by{|song| song.name}
+ #   puts "Which song number would you like to play?"
+ #   list_songs
+ #   index = gets.strip.to_i - 1
+ #
+ #   if index.between?(0, sorted_songs.length)
+ #     puts "Playing #{list_songs}"
+ #   end
+ # end
 
-   if index.between?(0, sorted_songs.length)
-     puts "Playing #{list_songs}"
-   end
- end
+ def play_song
+  puts "Which song number would you like to play?"
+  input = gets.chomp.to_i
+
+  # self.list_songs
+  songs = Song.all
+  # songs.sort {|a,b| a.name <=> b.name}
+
+
+
+  if (1..songs.length).include?(input)
+    song = Song.all.sort{ |a, b| a.name <=> b.name }[input - 1]
+  end
+  # binding.pry
+
+  puts "Playing #{song.name} by #{song.artist.name}" if song
+end
 
 
 
