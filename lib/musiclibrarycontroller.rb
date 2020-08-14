27ebf92f346581c667ea9d92e40d42e479757377
Kerri L OGorman <kerri.l.ogorman@gmail.com>
2020-08-14 19:33:56 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -1,7 +1,7 @@
 class MusicLibraryController
 
   def initialize(path = './db/mp3s') #Upon initialization, accepts an optional path to the library of MP3 files, defaulting to ./db/mp3s/
-
+    import = MusicImporter.new(path)
 
 
 end
