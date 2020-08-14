f6c888da23d3641678fa22fc52fc51e464f1fb68
Kerri L OGorman <kerri.l.ogorman@gmail.com>
2020-08-14 20:48:55 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -63,7 +63,7 @@ class MusicLibraryController
 
   def play_song
     puts "Which song number would you like to play?"
-    list_of_songs =  Song.all.sort{ |a, b| a.name <=> b.name }
+    list_of_songs = Song.all.sort{ |a, b| a.name <=> b.name }
     input = gets
 
     # songs = Song.all
