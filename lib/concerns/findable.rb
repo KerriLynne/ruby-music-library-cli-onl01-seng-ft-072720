139afa2124f81f16c64ef55b6a8d22843477882c
Kerri L OGorman <kerri.l.ogorman@gmail.com>
2020-08-14 18:51:57 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,7 +1,7 @@
 module Concerns::Findable
 
   def find_by_name(name)
-    self.all.detect {|song| song.name == name}
+    self.all.detect {|song| song.name == name} # is added as a class method to classes that extend the module/  isn't hard-coded/ works exactly like a generic version of Song.find_by_name,searching the extended class's @@all variable for an instance that matches the prov
   end  
 
 end
