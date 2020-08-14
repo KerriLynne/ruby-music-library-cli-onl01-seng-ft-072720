fdb0711c1136562a0da7c74154cb63e94cb3df53
Kerri L OGorman <kerri.l.ogorman@gmail.com>
2020-08-14 19:00:18 +0000

    Automatically backed up by Learn

--- lib/musicimporter.rb
+++ lib/musicimporter.rb
@@ -1,7 +1,7 @@
 class MusicImporter
-  attr_accessor :path
+  attr_accessor :path #accepts a file path to parse MP3 files from
 
-  def initialize(path)
+  def initialize(path) #retrieves the path provided to the MusicImporter object
     @path = path
   end
 
